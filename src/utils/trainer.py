f54fe44497a226818b930a5477bf7d46f6cf3d8d
aravinthakshan <aaravinth2005@gmail.com>
2025-01-13 12:01:07 +0530

    loss equal

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -111,7 +111,7 @@ def train(
                 contrastive_loss = contrastive_loss_fn(f1, f2)
                 
                 # Combined loss
-                loss = mse_loss + contrastive_loss*0.1
+                loss = mse_loss + contrastive_loss
                 
                 loss.backward()
                 optimizer.step()
