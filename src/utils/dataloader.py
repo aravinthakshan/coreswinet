27ad39cb6bf610c35faf3d134ca0bd2db1c9184e
aravinthakshan <aaravinth2005@gmail.com>
2025-01-15 23:59:28 +0530

    .

--- src/utils/dataloader.py
+++ src/utils/dataloader.py
@@ -218,7 +218,7 @@ class BSD400(Dataset):
 class Waterloo(Dataset):
     def __init__(self, root_dir, noise_level=25, crop_size=256, num_crops=32, normalize=True, tanfi=True):
         self.root_dir = root_dir
-        self.noise_level = f"noisy{noise_level}"
+        self.noise_level = f"noisy_{noise_level}"
         self.crop_size = crop_size
         self.num_crops = num_crops
         self.normalize = normalize
