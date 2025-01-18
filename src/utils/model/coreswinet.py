5e33048a054c0de6eb270109e103a0718e969a61
aravinthakshan <aaravinth2005@gmail.com>
2025-01-18 22:05:12 +0530

    .

--- src/utils/model/coreswinet.py
+++ src/utils/model/coreswinet.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import segmentation_models_pytorch as smp
 from torchsummary import summary
-# from utils.model.archs.SwinBlocks import SwinTransformerBlock
-# from utils.model.archs.AttentionModules import SimpleChannelAttention, SqueezeExcitationBlock
-# from utils.model.archs.ZSN2N import N2NNetwork
-from archs.SwinBlocks import SwinTransformerBlock
-from archs.AttentionModules import SimpleChannelAttention, SqueezeExcitationBlock
+from utils.model.archs.SwinBlocks import SwinTransformerBlock
+from utils.model.archs.AttentionModules import SimpleChannelAttention, SqueezeExcitationBlock
+from utils.model.archs.ZSN2N import N2NNetwork
+# from archs.SwinBlocks import SwinTransformerBlock
+# from archs.AttentionModules import SimpleChannelAttention, SqueezeExcitationBlock
 
 class PReLUBlock(nn.Module):
     def __init__(self, channels):
@@ -23,136 +23,81 @@ class PReLUBlock(nn.Module):
         return self.block(x)
     
 class Model(nn.Module):
-    def __init__(self, in_channels=3, contrastive=True, bypass=False):
+    def __init__(self, in_channels=3):
         super().__init__()
-        self.bypass = bypass
-
-        # First encoder (for noisy input)
-        self.unet1 = smp.Unet(
+        
+        # Initialize the base UNet model
+        self.unet = smp.Unet(
             encoder_name="resnet18",
-            encoder_weights="imagenet",
+            encoder_weights="imagenet", 
             in_channels=in_channels,
             classes=16,
             decoder_channels=(512, 256, 128, 64, 64),
         )
-
-        # Second encoder (for N2N denoised input)
-        self.unet2 = smp.Unet(
-            encoder_name="resnet18",
-            encoder_weights="imagenet",
-            in_channels=in_channels,
-            classes=16,
-            decoder_channels=(512, 256, 128, 64, 64),
-        )
-
-        self.encoder1 = self.unet1.encoder
-        self.encoder2 = self.unet2.encoder
-        self.decoder = self.unet1.decoder
-
-        encoder_channels = self.encoder1.out_channels
-
-        # Create Swin Transformer block for each encoder level
-        self.swin_blocks = nn.ModuleList([
+        
+        # Get the encoder from the UNet
+        self.encoder = self.unet.encoder
+        encoder_channels = self.encoder.out_channels
+        
+        # Create Swin Transformer blocks for skip connections
+        self.swin_skip_blocks = nn.ModuleList([
             SwinTransformerBlock(
-                dim=ch,
-                input_resolution=(256 // (2 ** i), 256 // (2 ** i)),
-                num_heads=min(8, max(1, ch // 32)),
-                window_size=min(7, max(3, ch // 32)),
+                dim=ch, 
+                input_resolution=(256 // (2**i), 256 // (2**i)),
+                num_heads=min(8, max(1, ch // 32)),  # Adaptive number of heads
+                window_size=min(7, max(3, ch // 32)),  # Adaptive window size
                 mlp_ratio=4.0
-            ) for i, ch in enumerate(encoder_channels)
+            ) for i, ch in enumerate(encoder_channels[:-1])
         ])
-
-        # Squeeze attention for bottleneck
+        
+        # Adaptive channel mapping for each skip connection
+        self.skip_adapters = nn.ModuleList([
+            nn.Conv2d(ch, ch, kernel_size=1) 
+            for ch in encoder_channels[:-1]
+        ])
+        
+        # Squeeze-and-Excitation Block for Bottleneck
         self.bottleneck_attention = SqueezeExcitationBlock(encoder_channels[-1])
-
-        # Contrastive heads
-        self.contrastive = contrastive
-        if contrastive:
-            self.contrastive_head1 = nn.Sequential(
-                nn.AdaptiveAvgPool2d(1),
-                nn.Flatten(),
-                nn.Linear(in_features=encoder_channels[-1], out_features=512),
-                nn.BatchNorm1d(512),
-                nn.ReLU(),
-                nn.Linear(in_features=512, out_features=64),
-                nn.BatchNorm1d(64),
-            )
-            self.contrastive_head2 = nn.Sequential(
-                nn.AdaptiveAvgPool2d(1),
-                nn.Flatten(),
-                nn.Linear(in_features=encoder_channels[-1], out_features=512),
-                nn.BatchNorm1d(512),
-                nn.ReLU(),
-                nn.Linear(in_features=512, out_features=64),
-                nn.BatchNorm1d(64),
-            )
-
+        
         # Final processing
         self.final = nn.Sequential(
             nn.Conv2d(64, 16, kernel_size=1),
             nn.BatchNorm2d(16),
-            nn.Conv2d(16, in_channels, kernel_size=1),
+            nn.Conv2d(16, 3, kernel_size=1),
             nn.Tanh()
         )
-
-    def process_features(self, feat1, feat2, swin_block):
-        if self.bypass:
-            # Skip element-wise max and directly process feat1 through Swin
-            B, C, H, W = feat1.shape
-            feat_reshaped = feat1.flatten(2).transpose(1, 2)
-            swin_out = swin_block(feat_reshaped)
-            return swin_out.transpose(1, 2).reshape(B, C, H, W)
-        else:
-            # Original processing with element-wise maximum
-            max_feat = torch.maximum(feat1, feat2)
-            B, C, H, W = max_feat.shape
-            feat_reshaped = max_feat.flatten(2).transpose(1, 2)
-            swin_out = swin_block(feat_reshaped)
-            return swin_out.transpose(1, 2).reshape(B, C, H, W)
-
-    def forward(self, x_noisy, x_n2n):
-        """
-        Forward pass of the model
-        Args:
-            x_noisy (torch.Tensor): Noisy input image
-            x_n2n (torch.Tensor): N2N denoised version of the input image
-        """
-        # Get features from first encoder
-        features1 = list(self.encoder1(x_noisy))
+    
+    def forward(self, x):
+        # Get features from original encoder
+        features = self.encoder(x)
         
-        # Get features from second encoder only if not bypassing
-        if not self.bypass:
-            features2 = list(self.encoder2(x_n2n))
-        else:
-            features2 = features1  # Dummy assignment, won't be used
-
-        # Process each encoder level
+        # Apply Squeeze-and-Excitation to bottleneck
+        bottleneck_feat = self.bottleneck_attention(features[-1])
+        
+        # Process skip connections with Swin Transformer blocks
         processed_features = []
-        for i in range(len(features1)):
-            processed_feat = self.process_features(
-                features1[i], 
-                features2[i], 
-                self.swin_blocks[i]
-            )
-            processed_features.append(processed_feat)
-
-        # The last processed feature becomes the bottleneck
-        bottleneck = self.bottleneck_attention(processed_features[-1])
-
-        # Pass processed features into decoder as skip connections
-        decoder_features = processed_features[:-1]
-        decoder_output = self.decoder(*decoder_features, bottleneck)
-
+        for feat, swin_block, adapter in zip(features[:-1], self.swin_skip_blocks, self.skip_adapters):
+            # Reshape for transformer
+            B, C, H, W = feat.shape
+            feat_reshaped = feat.flatten(2).transpose(1, 2)
+            
+            # Apply Swin Transformer block
+            transformed_feat = swin_block(feat_reshaped)
+            
+            # Reshape back and apply channel adapter
+            transformed_feat = transformed_feat.transpose(1, 2).reshape(B, C, H, W)
+            transformed_feat = adapter(transformed_feat)
+            
+            processed_features.append(transformed_feat)
+        
+        # Use decoder with processed features
+        decoder_output = self.unet.decoder(*processed_features, bottleneck_feat)
+        
+        # Final processing
         output = self.final(decoder_output)
-
-        if self.contrastive:
-            f1 = self.contrastive_head1(features1[-1])
-            if self.bypass:
-                f2 = self.contrastive_head2(features1[-1])  # Use features1 when bypassing
-            else:
-                f2 = self.contrastive_head2(features2[-1])
-            return output, f1, f2
+        
         return output
+
 
 
 # ### ----->>> DO NOT DELETE THIS ( MULTIPLE 1,1,2,2,3 BLOCKS OF SWIN PER RESIDUAL ) <<<----------
