c53248a282e4356c0fb2aebab851f7698d1940da
aravindshenoy13 <aravindshenoywork@gmail.com>
2025-01-21 18:36:43 +0530

    fuck your code aravinth; git push

--- src/utils/model/coreswinet.py
+++ src/utils/model/coreswinet.py
@@ -97,7 +97,14 @@ class Model(nn.Module):
         )
 
     def process_features(self, feat1, feat2, swin_block):
-        if self.bypass_second:
+        if self.bypass_first:
+            # Skip element-wise max and directly process feat2 through Swin
+            B, C, H, W = feat2.shape
+            feat_reshaped = feat2.flatten(2).transpose(1, 2)
+            swin_out = swin_block(feat_reshaped)
+            return swin_out.transpose(1, 2).reshape(B, C, H, W)
+
+        elif self.bypass_second:
             # Skip element-wise max and directly process feat1 through Swin
             B, C, H, W = feat1.shape
             feat_reshaped = feat1.flatten(2).transpose(1, 2)
