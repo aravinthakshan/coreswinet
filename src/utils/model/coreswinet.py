37fd7e571f4a0c25293faa7b75b7024806317de9
aryamangupta04 <66553795+aryamangupta04@users.noreply.github.com>
2025-01-27 17:57:42 +0530

    change

--- src/utils/model/coreswinet.py
+++ src/utils/model/coreswinet.py
@@ -126,8 +126,10 @@ class Model(nn.Module):
         #     )
         for i in range(len(features1)):
             if self.bypass:
+                
                 processed_features.append(self.attention_blocks[i](features1[i]))
             else:
+                print(features1[i].shape)
                 max_feat = torch.maximum(features1[i], features2[i])
                 processed_features.append(self.attention_blocks[i](max_feat))
         # The last processed feature becomes the bottleneck
