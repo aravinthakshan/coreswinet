130972111687dbbc75dee440b402e1cf03b8a62d
aravinthakshan <aaravinth2005@gmail.com>
2025-01-27 22:15:19 +0530

    .

--- src/utils/dataloader.py
+++ src/utils/dataloader.py
@@ -290,8 +290,8 @@ class Waterloo(Dataset):
                 self.image_pairs.append((noisy_crop, clean_crop))
                 
     def __len__(self):
-        return 100
-        # return len(self.image_pairs)
+        # return 100
+        return len(self.image_pairs)
 
     def __getitem__(self, idx):
         noisy, clean = self.image_pairs[idx]
