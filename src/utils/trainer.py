0d42a0cb2116e63ef5dd599fab5d0974481588a2
aravinthakshan <aaravinth2005@gmail.com>
2025-02-04 17:54:37 +0530

    .

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -57,7 +57,7 @@ def train(
     # Initialize main model with bypass parameter
     model = Model(contrastive=True, bypass=False).to(device)
     model = replace_decoder_convs(model)
-
+    model.to(device)
     # Optimizer
     optimizer = SOAP(
         model.parameters(),
