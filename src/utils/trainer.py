49884c858874dd9d44c1e0c0cf232af68c1dd7a8
aryamangupta04 <66553795+aryamangupta04@users.noreply.github.com>
2025-02-06 11:00:26 +0530

    change

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -41,7 +41,7 @@ def train(
         train_dataset = SIDD(data_dir=train_dir, normalize=True, standardize=False, mode = 'train')
         val_dataset = SIDD(data_dir='/kaggle/input/sidd-val', normalize=True, standardize=False, mode = 'val')
     elif dataset_name=='rain13k':
-        dataset = rain13k(root_dir=train_dir, noise_level=noise_level, crop_size=128, num_crops=2, normalize=True,augmentation=get_training_augmentation())
+        dataset = rain13k(root_dir=train_dir, noise_level=noise_level, crop_size=128, num_crops=1, normalize=True,augmentation=get_training_augmentation())
 
     if dataset_name!='SIDD':
         train_size = int(0.8 * len(dataset))
