9c323e3030263df927204c84b715fb99269bfa56
aryamangupta04 <66553795+aryamangupta04@users.noreply.github.com>
2025-02-07 18:00:56 +0530

    change

--- src/utils/dataloader.py
+++ src/utils/dataloader.py
@@ -1187,7 +1187,7 @@ class rain13k(Dataset):
 
 
 class uiebd_dataset(Dataset):
-    def __init__(self, root_dir, noise_level=25, crop_size=256, num_crops=32, normalize=True, tanfi=True):
+    def __init__(self, root_dir, noise_level=25, crop_size=256, num_crops=32, normalize=True, tanfi=True,augmentation=None):
         self.root_dir = root_dir
         self.noise_level = f"noisy_{noise_level}"
         self.crop_size = crop_size
@@ -1220,9 +1220,16 @@ class uiebd_dataset(Dataset):
             for _ in range(self.num_crops):
                 top = random.randint(0, h - self.crop_size)
                 left = random.randint(0, w - self.crop_size)
-
+                
                 clean_crop = clean_np[top:top + self.crop_size, left:left + self.crop_size]
                 noisy_crop = noisy_np[top:top + self.crop_size, left:left + self.crop_size]
+            
+                if self.augmentation:
+                    augmented = self.augmentation(image=noisy_crop, image1=clean_crop)
+                    noisy_crop, clean_crop = augmented['image'], augmented['image1']
+
+                clean_crop = torch.from_numpy(clean_crop).permute(2, 0, 1)
+                noisy_crop = torch.from_numpy(noisy_crop).permute(2, 0, 1)
 
                 clean_crop = torch.from_numpy(clean_crop).permute(2, 0, 1)
                 noisy_crop = torch.from_numpy(noisy_crop).permute(2, 0, 1)
