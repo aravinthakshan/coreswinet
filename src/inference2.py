9df0bfd762681399cfc0254f3e348a42ba6d3307
aravinthakshan <aaravinth2005@gmail.com>
2025-02-09 05:35:24 -0500

    .

--- src/inference2.py
+++ src/inference2.py
@@ -135,7 +135,7 @@ def test(
     
     # Load model
     main_model = load_models(
-        '/kaggle/input/somethingaravinthakshan/pytorch/default/1/final_model.pth',
+        './main_model/final_model.pth',
         device
     )
     main_model.to(device).eval()
